903820ebdf5b202921550144f669720072c5862f
ermiyasdb <ermiyas43@hotmail.com>
2023-06-19 10:02:40 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-
+#include2 <stdio.h>
 /**
- * main - main block
+ * main - Entry point
  *
- * Return: Always 0
+ * Return: Always(0)
  */
 int main(void)
-{
-    printf("with proper grammar, but the outcome is a piece of art,/n");
-Return(0);
+{								
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0)
 }
-
